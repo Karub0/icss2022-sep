a1eadf1499567dd99bfda0886471551aa5651992
Burak <burakbayramnl@hotmail.com>
2025-10-24 16:56:29 +0200

    checker stylesheet/rule

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,7 +1,7 @@
 package nl.han.ica.icss.checker;
 
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -13,9 +13,35 @@ public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
+        variableTypes = new HANLinkedList<>();
 
     }
+
+    private void checkStylesheet(Stylesheet sheet) {
+        for (ASTNode child : sheet.getChildren()) {
+            if (child instanceof VariableAssignment) {
+                checkVariableAssignment((VariableAssignment) child);
+            } else if (child instanceof Stylerule) {
+                checkStylerule((Stylerule) child);
+            }
+        }
+    }
+
+    private void checkStylerule(Stylerule rule) {
+        variableTypes.addFirst(new HashMap<>());
+
+        for (ASTNode child : rule.getChildren()) {
+            if (child instanceof Declaration) {
+                checkDeclaration((Declaration) child);
+            } else if (child instanceof IfClause) {
+                checkIfClause((IfClause) child);
+            }
+        }
+
+        variableTypes.removeFirst();
+    }
+
+
 
     
 }
