de8a5683da1e6ca18c0a699d071e4b2770076933
Burak <burakbayramnl@hotmail.com>
2025-10-25 16:35:17 +0200

    declaration & type

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -59,7 +59,69 @@ public class Checker {
         variableTypes.getFirst().put(assignment.name.name, type);
     }
 
+    private void checkDeclaration(Declaration declaration) {
+        ExpressionType type = getExpressionType(declaration.expression);
+
+        String prop = declaration.property.name;
+        if ((prop.equals("width") || prop.equals("height"))
+                && !(type == ExpressionType.PIXEL || type == ExpressionType.PERCENTAGE || type == ExpressionType.SCALAR)) {
+            declaration.setError("Property '" + prop + "' expects numeric value.");
+        }
+
+        if ((prop.equals("color") || prop.equals("background-color"))
+                && type != ExpressionType.COLOR) {
+            declaration.setError("Property '" + prop + "' expects color value.");
+        }
+    }
+
+    private ExpressionType getExpressionType(ASTNode type) {
+        if (type instanceof ColorLiteral) return ExpressionType.COLOR;
+        if (type instanceof PixelLiteral) return ExpressionType.PIXEL;
+        if (type instanceof PercentageLiteral) return ExpressionType.PERCENTAGE;
+        if (type instanceof ScalarLiteral) return ExpressionType.SCALAR;
+        if (type instanceof BoolLiteral) return ExpressionType.BOOL;
+
+        if (type instanceof VariableReference) {
+            VariableReference reference = (VariableReference) type;
+
+            for (int i = 0; i < variableTypes.getSize(); i++) {
+                HashMap<String, ExpressionType> scope = variableTypes.get(i);
+                if (scope.containsKey(reference.name)) {
+                    return scope.get(reference.name);
+                }
+            }
+        }
+
+        if (type instanceof Operation) {
+            Operation operation = (Operation) type;
+            ExpressionType left = getExpressionType(operation.lhs);
+            ExpressionType right = getExpressionType(operation.rhs);
+
+            if (left == ExpressionType.COLOR || right == ExpressionType.COLOR) {
+                operation.setError("Color cannot be used in operations.");
+                return ExpressionType.UNDEFINED;
+            }
+
+            if (operation instanceof AddOperation || operation instanceof SubtractOperation) {
+                if (left != right) {
+                    operation.setError("Operand + or - must have same type.");
+                    return ExpressionType.UNDEFINED;
+                }
+                return left;
+
+            } else if (operation instanceof MultiplyOperation) {
+                if (left == ExpressionType.SCALAR) return right;
+                if (right == ExpressionType.SCALAR) return left;
+                operation.setError("One operand of * must be scalar.");
+                return ExpressionType.UNDEFINED;
+            }
+        }
+
+        return ExpressionType.UNDEFINED;
+    }
 
 
-    
+
+
+
 }
