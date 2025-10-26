c83f02b1c4b990954494aa96336e00c835d9d755
Burak <burakbayramnl@hotmail.com>
2025-10-26 14:55:16 +0100

    generator GEO2

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,31 +1,89 @@
 package nl.han.ica.icss.generator;
 
-
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.Declaration;
-import nl.han.ica.icss.ast.Stylerule;
-import nl.han.ica.icss.ast.Stylesheet;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        return generateStylesheet(ast.root);
-
-
+		return generateStylesheet(ast.root, 0);
 	}
 
-    private String generateStylesheet(Stylesheet node) {
-		return generateStylerule((Stylerule)node.getChildren().get(0));
-    }
+	private String generateStylesheet(Stylesheet sheet, int indentLevel) {
+		StringBuilder builder = new StringBuilder();
 
-	private String generateStylerule(Stylerule node) {
-		return node.selectors.get(0).toString()
-				+ "{\n"
-				+ generateDeclaration((Declaration)node.body.get(0))
-		        + "}";
+		for (ASTNode child : sheet.getChildren()) {
+			if (child instanceof Stylerule) {
+				builder.append(generateStylerule((Stylerule) child, indentLevel)).append("\n");
+			}
+		}
+
+		return builder.toString();
 	}
 
-	private String generateDeclaration(Declaration node) {
-		return "h";
+	private String generateStylerule(Stylerule rule, int indentLevel) {
+		StringBuilder builder = new StringBuilder();
+		String indent = "  ".repeat(indentLevel);
+		String indent2 = "  ".repeat(indentLevel + 1);
+
+		for (int i = 0; i < rule.selectors.size(); i++) {
+			builder.append(indent).append(rule.selectors.get(i).toString());
+			if (i < rule.selectors.size() - 1) {
+				builder.append(", ");
+			}
+		}
+		builder.append(" {\n");
+
+		for (ASTNode child : rule.body) {
+			if (child instanceof Declaration) {
+				builder.append(indent2).append(generateDeclaration((Declaration) child)).append("\n");
+
+			} else if (child instanceof Stylerule) {
+				builder.append(generateStylerule((Stylerule) child, indentLevel + 1)).append("\n");
+			}
+		}
+
+		builder.append(indent).append("}");
+		return builder.toString();
+	}
+
+	private String generateDeclaration(Declaration declaration) {
+		return declaration.property.name + ": " + literalToString(declaration.expression) + ";";
+	}
+
+	private String literalToString(ASTNode expression) {
+		if (expression instanceof PixelLiteral) {
+			return ((PixelLiteral) expression).value + "px";
+
+		} else if (expression instanceof PercentageLiteral) {
+			return ((PercentageLiteral) expression).value + "%";
+
+		} else if (expression instanceof ScalarLiteral) {
+			return String.valueOf(((ScalarLiteral) expression).value);
+
+		} else if (expression instanceof ColorLiteral) {
+			return ((ColorLiteral) expression).value;
+
+		} else if (expression instanceof BoolLiteral) {
+			return String.valueOf(((BoolLiteral) expression).value).toLowerCase();
+
+		} else if (expression instanceof VariableReference) {
+			return ((VariableReference) expression).name;
+
+		} else if (expression instanceof Operation) {
+			Operation operation = (Operation) expression;
+			String left = literalToString(operation.lhs);
+			String right = literalToString(operation.rhs);
+			if (operation instanceof nl.han.ica.icss.ast.operations.AddOperation) {
+				return left + " + " + right;
+
+			} else if (operation instanceof nl.han.ica.icss.ast.operations.SubtractOperation) {
+				return left + " - " + right;
+
+			} else if (operation instanceof nl.han.ica.icss.ast.operations.MultiplyOperation) {
+				return left + " * " + right;
+			}
+		}
+		return "";
 	}
 }
